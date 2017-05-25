05c9885caecb689abf5198e62546012f0649becf
li_zhil <li_zhil@worksap.co.jp>
2017-05-25 13:58:05 +0800

    ...modify jump v

--- js/global.js
+++ js/global.js
@@ -13,12 +13,12 @@ var renderer, stage, skyContainer, groundContainer, textContainer;
 const W = keyboard(87), SPACE = keyboard(32);
 var restartMessage, scoreMessage, buffMessage;
 const startVelocity = suitWindowSize(4);
-const startJumpV = suitWindowSize(16);
+const startJumpV = suitWindowSize(10);
 var stoneArray, starArray;
 var animation;
 var score, level, velocity, jumping, groundY, jumpV = startJumpV;
 
-const jumpVAdd = suitWindowSize(5);
+const jumpVAdd = suitWindowSize(4);
 var starBuffStartTime = undefined;
 var masterJumpTag = false;
 var starsGot = 0;
